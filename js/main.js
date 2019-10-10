69d6ee07aaf70f12ed010c5de8ca534bf5a27d50
Daniel Barber <d.barber@aleph-1.com>
2019-10-10 10:27:19 -0400

    Updates to style and docs

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
-console.log( Cinder );
-
 var words = 'lorem ipsum dolor nabet sit amet consectetur adipiscing elit ad quorum et cognitionem et usum iam corroborati natura ipsa praeeunte deducimur ratio quidem vestra sic cogit cur nisi quod turpis oratio est atqui reperies inquit in hoc quidem pertinacem si quicquam extra virtutem habeatur in bonis iam id ipsum absurdum maximum malum neglegi quamquam te quidem video minime esse deterritum duo reges constructio interrete'.split( ' ' ),
+	colors = '#F2ACC9,#9AAFE4,#BB98F6,#C3D38C,#E1B79E,#90E2DC'.split( ',' ),
 	columns = 3,//'400px',//'400px',
-	gap = 6,
-	colors = '#F2ACC9,#9AAFE4,#BB98F6,#C3D38C,#E1B79E,#90E2DC'.split( ',' );
+	gap = 6;
 
+// ------------------------------
+// HELPER FUNCTIONS
+// ------------------------------
+
+// assign events to HTML elements or collections
 function on( elements, events ){
 	if( !elements.length ) elements = [ elements ];
 	for( var i = 0; i < elements.length; i++ )
@@ -17,7 +20,43 @@ function on( elements, events ){
 	}
 }
 
-// tint .btn elements a random color on mouseover
+// get multiple attributes from elements
+function getAttributes( element, attributes )
+{
+	var output = {};
+	for( var i = 0; i < attributes.length; i++ )
+	{
+		var attribute = attributes[ i ];
+		output[ attribute ] = element.getAttribute( attribute );
+	}
+	return output;
+}
+
+// ------------------------------
+// LOGO ANIMATION
+// ------------------------------
+
+var rects = document.getElementsByTagName( 'rect' ),
+	rectDimensions = [],
+	rectAttributes = 'x,y,width,height'.split( ',' );
+
+for( var i = 0; i < rects.length; i++ )
+{	
+	rectDimensions[ i ] = getAttributes( rects[ i ], rectAttributes );
+}
+
+on( document.getElementsByTagName( 'svg' ), {
+	mouseover: function( e )
+	{
+		console.log( 'logo mouseover' );
+		//for( var i = 0; i)
+	}
+} );
+
+// ------------------------------
+// TINT BUTTONS A RANDOM COLOR
+// ------------------------------
+
 on( document.getElementsByClassName( 'btn' ), {
 	mouseover: function( e )
 	{
